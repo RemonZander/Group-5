bd69088c47cc1e54bb5ebaf3d8af6e6dae74ec7b
Rémon Zander (0948619) <0948619@hr.nl>
2021-02-15 19:59:50 +0100

    made textbox read-only and desabled

--- testing/Form1.Designer.cs
+++ testing/Form1.Designer.cs
@@ -45,8 +45,10 @@ namespace testing
             // 
             // textBox1
             // 
+            this.textBox1.Enabled = false;
             this.textBox1.Location = new System.Drawing.Point(476, 287);
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(100, 23);
             this.textBox1.TabIndex = 1;
             // 
