c0c7b1ae307338fb07e96c8370db32cf05316531
simon prinsen <simon360@hotmail.nl>
2021-03-22 11:17:14 +0100

    added Json library added database saving system???

--- restaurant/restaurant/IO.cs
+++ restaurant/restaurant/IO.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json;
 
 namespace restaurant
 {
     public class IO
     {
+        public void Savedatabase(Database database)
+        {
+            if (!FileSystem.DirectoryExists(@"..\database\"))
+            {
+                FileSystem.CreateDirectory(@"..\database\");
+            }
+
+            string output = JsonConvert.SerializeObject(database, Formatting.Indented);
+        }
     }
 }
