3c098b78447b31a339bbacd08159c159447fadb5
Ludo de Vries <ludo2000@live.nl>
2021-03-22 12:13:57 +0100

    Added klantgegevens try

--- restaurant/restaurant/Code_Eigenaar_menu.cs
+++ restaurant/restaurant/Code_Eigenaar_menu.cs
@@ -9,14 +9,34 @@ namespace restaurant
     {
         private Database database = new Database();
         Reserveringen[] remon = new Reserveringen[100];
+        IO io = new IO();
         public void fillReservations()
         {
             for (int i = 0; i < 100; i++)
             {
-                remon[i].datum = new DateTime(2000-i, 1, 14);
+                long[] telefoonnummer = new long[1];
+                telefoonnummer[0] = 0613122228;
+                Klantgegevens[] klantgegevens = new Klantgegevens[1];
+                klantgegevens[0].voornaam = "Rémon";
+                klantgegevens[0].achternaam = "Zander";
+                klantgegevens[0].adres = new adres
+                {
+                    land = "NL",
+                    huisnummer = 6,
+                    postcode = "3233XK",
+                    straatnaam = "Zeehoeveweg",
+                    woonplaats = "Oostvoorne"
+                };
+                klantgegevens[0].klantnummer = 0948619 - i;
+                klantgegevens[0].leeftijd = 21 * i;
+                klantgegevens[0].telefoonnummer = telefoonnummer;
+                remon[i].klantgegevens = klantgegevens;
+                remon[i].datum = new DateTime(2000 - i, 1, 14);
+        
             }
 
             database.reserveringen = remon;
+            io.Savedatabase(database);
         }
         public string sayHello()
         {
