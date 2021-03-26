df087609fb3cd0bacd6a906f60852f865e35fe67
Ludo de Vries <ludo2000@live.nl>
2021-03-26 21:15:31 +0100

    Added popular dishes

--- restaurant/restaurant/Code_Eigenaar_menu.cs
+++ restaurant/restaurant/Code_Eigenaar_menu.cs
@@ -8,64 +8,37 @@ namespace restaurant
     public class Code_Eigenaar_menu
     {
         private Database database = new Database();
-        Reserveringen[] remon = new Reserveringen[100];
         IO io = new IO();
-        public void fillReservations()
+
+        public Code_Eigenaar_menu()
         {
-            for (int i = 0; i < 100; i++)
-            {
-                long[] telefoonnummer = new long[1];
-                telefoonnummer[0] = 0613122228;
-                Klantgegevens[] klantgegevens = new Klantgegevens[2];
-                klantgegevens[0] = new Klantgegevens { 
-                    voornaam = "Rémon",
-                    achternaam = "Zander",
-                    adres = new adres
-                    {
-                        land = "NL",
-                        huisnummer = 6,
-                        postcode = "3233XK",
-                        straatnaam = "Zeehoeveweg",
-                        woonplaats = "Oostvoorne"
-                    },
-                    klantnummer = 0948619 - i,
-                    geb_datum = new DateTime(2000 + i, 10, 20),
-                    telefoonnummer = telefoonnummer,
-                };
-
-                telefoonnummer[0] = 0681295232;
-                klantgegevens[1] = new Klantgegevens
-                {
-                    voornaam = "Ludo",
-                    achternaam = "de Vries",
-                    adres = new adres
-                    {
-                        land = "NL",
-                        huisnummer = 9,
-                        postcode = "3311JG",
-                        straatnaam = "Burgemeester de Raadtsingel",
-                        woonplaats = "Dordrecht"
-                    },
-                    klantnummer = 1013007 - i,
-                    geb_datum = new DateTime(2000 + i, 10, 20),
-                    telefoonnummer = telefoonnummer,
-                };
-                remon[i].klantgegevens = klantgegevens;
-                remon[i].datum = new DateTime(2000 - i, 1, 14);
-            }
-
-            database.reserveringen = remon;
-            io.Savedatabase(database);
+            database = io.Getdatabase();
         }
-        public string sayHello()
+
+        public List<Tuple<Gerechten, int>> GetUserOrderInfo(DateTime beginDate, DateTime endDate)
         {
-            string s = "";
-            foreach (var item in database.reserveringen)
+            List<Tuple<Gerechten, int>> populaireGerechten = new List<Tuple<Gerechten, int>>();
+            //hier komt een loop waarbij populaire gerechten gevuld wordt met alle bestaande gerechten.
+            //als de reservering plaats heeft gevonden binnen de gestelde tijd.
+            foreach (var reservering in database.reserveringen)
             {
-                s += item;
-                s += Environment.NewLine;
+                if (reservering.datum.Date >= beginDate.Date && reservering.datum.Date <= endDate.Date)
+                {
+                    foreach (var gerecht in reservering.gerechten)
+                    {
+                        for (int i = 0; i < populaireGerechten.Count; i++)
+                        {
+                            if (populaireGerechten[i].Item1.Equals(gerecht))
+                            {
+                                populaireGerechten[i] = Tuple.Create(gerecht, populaireGerechten[i].Item2 + 1);
+                                break;
+                            }
+                        }
+
+                    }
+                }
             }
-            return s;
+            return populaireGerechten;
         }
     }
 }
