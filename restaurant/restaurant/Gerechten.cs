70db0bcb10e768e50bfcc72766aca8674dd71284
Rémon Zander (0948619) <0948619@hr.nl>
2021-03-27 16:43:36 +0100

    added name and ID to gerechten

--- restaurant/restaurant/Gerechten.cs
+++ restaurant/restaurant/Gerechten.cs
@@ -7,6 +7,10 @@ namespace restaurant
 {
     public struct Gerechten
     {
+        public string naam { get; set; }
+
+        public int ID { get; set; }
+
         public List<string> ingredienten { get; set; }
 
         public bool is_populair { get; set; }
