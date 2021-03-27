4ce9c870487d690da04a2805364cb92e6d5ad1c5
Ludo de Vries <ludo2000@live.nl>
2021-03-27 17:26:36 +0100

    Added fillreservations Added debug popular dishes

--- restaurant/restaurant/Code_Eigenaar_menu.cs
+++ restaurant/restaurant/Code_Eigenaar_menu.cs
@@ -17,7 +17,81 @@ namespace restaurant
 
         public void Debug()
         {
+            DateTime endDate = DateTime.Now;
+            endDate = endDate.AddDays(7);
+            FillReservations();
+            GetUserOrderInfo(DateTime.Now, endDate);
+        }
 
+        private void FillReservations()
+        {
+            List<Reserveringen> reserveringen = new List<Reserveringen>();
+            for (int i = 0; i < 100; i++)
+            {
+                List<Tafels> tafelsTemp = new List<Tafels>();
+                Random rndTafels = new Random();
+                for (int j = 0; j < rndTafels.Next(4); j++)
+                {
+                    Tafels temp = new Tafels
+                    {
+                        Zetels = 4,
+                        ID = rndTafels.Next(101)
+                    };
+                    if (temp.ID % 2 != 0)
+                    {
+                        temp.isRaam = true;
+                    }
+                    tafelsTemp.Add(temp);
+                }
+
+                List<Gerechten> gerechtenList = new List<Gerechten>();
+                Random rndGerechten = new Random();
+                for (int z = 0; z < 4; z++)
+                {
+                    int x = rndGerechten.Next(6);
+                    Gerechten gerechtenTemp = new Gerechten();
+                    gerechtenTemp.ID = x;
+                    if (x == 0)
+                    {
+                        gerechtenTemp.naam = "Pizza Salami";
+                    }
+                    else if (x == 1)
+                    {
+                        gerechtenTemp.naam = "Vla";
+                    }
+                    else if (x == 2)
+                    {
+                        gerechtenTemp.naam = "Hamburger";
+                    }
+                    else if (x == 3)
+                    {
+                        gerechtenTemp.naam = "Yoghurt";
+                    }
+                    else if (x == 4)
+                    {
+                        gerechtenTemp.naam = "IJs";
+                    }
+                    else if (x == 5)
+                    {
+                        gerechtenTemp.naam = "Patat";
+                    }
+                    gerechtenList.Add(gerechtenTemp);
+                }
+
+                Random rndDay = new Random();
+                DateTime datum = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, rndDay.Next(9, 23), 0, 0);
+                datum.AddDays(rndDay.Next(8));
+                Reserveringen tempRes = new Reserveringen
+                {
+                    ID = i,
+                    tafels = tafelsTemp,
+                    datum = datum,
+                    gerechten = gerechtenList
+                };
+
+                reserveringen.Add(tempRes);
+            }
+            database.reserveringen = reserveringen;
         }
 
         public List<Tuple<Gerechten, int>> GetUserOrderInfo(DateTime beginDate, DateTime endDate)
@@ -25,6 +99,35 @@ namespace restaurant
             List<Tuple<Gerechten, int>> populaireGerechten = new List<Tuple<Gerechten, int>>();
             //hier komt een loop waarbij populaire gerechten gevuld wordt met alle bestaande gerechten.
             //als de reservering plaats heeft gevonden binnen de gestelde tijd.
+            populaireGerechten.Add(Tuple.Create(new Gerechten { 
+                ID = 0,
+                naam = "Pizza Salami",
+            }, 0));
+            populaireGerechten.Add(Tuple.Create(new Gerechten
+            {
+                ID = 1,
+                naam = "Vla",
+            }, 0));
+            populaireGerechten.Add(Tuple.Create(new Gerechten
+            {
+                ID = 2,
+                naam = "Hamburger",
+            }, 0));
+            populaireGerechten.Add(Tuple.Create(new Gerechten
+            {
+                ID = 3,
+                naam = "Yoghurt",
+            }, 0));
+            populaireGerechten.Add(Tuple.Create(new Gerechten
+            {
+                ID = 4,
+                naam = "IJs",
+            }, 0));
+            populaireGerechten.Add(Tuple.Create(new Gerechten
+            {
+                ID = 5,
+                naam = "Patat",
+            }, 0));
             foreach (var reservering in database.reserveringen)
             {
                 if (reservering.datum.Date >= beginDate.Date && reservering.datum.Date <= endDate.Date)
@@ -39,6 +142,7 @@ namespace restaurant
                                 break;
                             }
                         }
+                        
 
                     }
                 }
