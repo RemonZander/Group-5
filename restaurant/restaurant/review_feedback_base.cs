7b973d5306a6b6ef284dded94c5018a0aa94cb84
Rémon Zander (0948619) <0948619@hr.nl>
2021-03-28 17:52:34 +0200

    added data structure for reviews and feedback

---
+++ restaurant/restaurant/review_feedback_base.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace restaurant
+{
+    public abstract class review_feedback_base
+    {
+        public int ID { get; set; }
+
+        public Klantgegevens klantgegevens { get; set; }
+
+        public string message { get; set; }
+    }
+}
