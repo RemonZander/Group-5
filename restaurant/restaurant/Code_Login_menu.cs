3ea3b698fcea92a3741b3f85be104bd3cdc078c9
luenn <luenn@LAPTOP-0QBTVCJ2>
2021-03-29 14:09:52 +0200

    Login and register funcitons

--- restaurant/restaurant/Code_Login_menu.cs
+++ restaurant/restaurant/Code_Login_menu.cs
@@ -7,8 +7,54 @@ namespace restaurant
 {
     public class Code_Login_menu
     {
-        private IO IO;
+        private IO IO = new IO();
+        
+        private Database database = new Database();
 
-        private Database database;
+        public Code_Login_menu()
+        {
+            database = IO.Getdatabase();
+        }
+
+        public void Debug()
+        {
+
+        }
+
+        public Login_gegevens Login_Check(string email, string password)
+        {
+            foreach (var login_Gegevens in database.login_gegevens)
+            {
+                if (email == login_Gegevens.email && password == login_Gegevens.password)
+                {
+                    return login_Gegevens;
+                }
+            }
+            return new Login_gegevens
+            {
+                type = "No account found"
+            };
+        }
+
+        public string Register(Login_gegevens login_Gegevens)
+        {
+            List<Login_gegevens> List_Login_temp = database.login_gegevens;
+
+            foreach (var login_temp in database.login_gegevens)
+            {
+                if (login_Gegevens.email == login_temp.email && login_Gegevens.type == login_temp.type)
+                {
+                    return "Account already exists";
+                }
+                else
+                {
+                    List_Login_temp.Add(login_Gegevens);
+                    database.login_gegevens = List_Login_temp;
+                    IO.Savedatabase(database);
+                    return "Account created";
+                }
+            }
+            return "Unexpected error";
+        }
     }
 }
