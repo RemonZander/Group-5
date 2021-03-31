92970eb90eb406a6b574cbfa9c2244d3bb0fbc6a
Rémon Zander (0948619) <0948619@hr.nl>
2021-03-31 17:10:51 +0200

    added testing class and added a few functions to it

---
+++ restaurant/restaurant/testing class.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace restaurant
+{
+    public class Testing_class
+    {
+        private Database database = new Database();
+        private readonly IO io = new IO();
+
+        public Testing_class()
+        {
+            database = io.Getdatabase();
+        }
+        
+        public void Debug()
+        {
+
+        }
+
+        public void Fill_reservations(int amount)
+        {
+            List<Reserveringen> reserveringen_list = new List<Reserveringen>();
+            Random rnd = new Random();
+            for (int a = 0; a < amount; a++)
+            {
+                List<Tafels> tafels = new List<Tafels>();
+                for (int b = 0; b < rnd.Next(4); b++)
+                {
+                    tafels.Add(database.tafels[b]);
+                }
+
+                reserveringen_list.Add(new Reserveringen
+                {
+                    datum = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + rnd.Next(-30, 30), 22, 0, 0),
+                    ID = a,
+                    gerechten = Make_dishes(),
+                    tafels = tafels,
+                    
+                });
+            }
+
+            database.reserveringen = reserveringen_list;
+            io.Savedatabase(database);
+        }
+
+        public void Fill_reservations(int amount, List<DateTime> datum, List<Gerechten> gerechten, List<Tafels> tafels)
+        {
+            List<Reserveringen> reserveringen_list = new List<Reserveringen>();
+            for (int a = 0; a < amount; a++)
+            {
+                reserveringen_list.Add(new Reserveringen
+                {
+
+                });
+            }
+        }
+
+        public List<Gerechten> Make_dishes()
+        {
+            List<Gerechten> gerechten = new List<Gerechten>();
+
+            gerechten.Add(new Gerechten
+            {
+                ID = 0,
+                naam = "Pizza Salami",
+                is_populair = true,
+                is_gearchiveerd = false,
+                special = true,
+                prijs = 15.0
+            });
+            gerechten.Add(new Gerechten
+            {
+                ID = 1,
+                naam = "Vla",
+                is_populair = false,
+                is_gearchiveerd = false,
+                special = true,
+                prijs = 8.0
+            });
+            gerechten.Add(new Gerechten
+            {
+                ID = 2,
+                naam = "Hamburger",
+                is_populair = true,
+                is_gearchiveerd = false,
+                special = false,
+                prijs = 13.0
+            });
+            gerechten.Add(new Gerechten
+            {
+                ID = 3,
+                naam = "Yoghurt",
+                is_populair = false,
+                is_gearchiveerd = true,
+                special = false,
+                prijs = 6.0
+            });
+            gerechten.Add(new Gerechten
+            {
+                ID = 4,
+                naam = "IJs",
+                is_populair = false,
+                is_gearchiveerd = true,
+                special = false,
+                prijs = 9.5
+            });
+            gerechten.Add(new Gerechten
+            {
+                ID = 5,
+                naam = "Patat",
+                is_populair = true,
+                is_gearchiveerd = false,
+                special = false,
+                prijs = 11.5
+            });
+
+            return gerechten;
+        }
+
+        public List<Gerechten> Make_dishes(List<string> names, List<bool> populair, List<bool> archived, List<bool> special, List<double> price)
+        {
+            if (populair.Count != names.Count || archived.Count != names.Count || special.Count != names.Count || price.Count != names.Count)
+            {
+                List<Gerechten> leeg = new List<Gerechten>();
+                return leeg;
+            }
+
+            List<Gerechten> gerechten = new List<Gerechten>();
+            for (int a = 0; a < names.Count; a++)
+            {
+                gerechten.Add(new Gerechten
+                {
+                    naam = names[a],
+                    ID = a,
+                    special = special[a],
+                    is_gearchiveerd = archived[a],
+                    is_populair = populair[a],
+                    prijs = price[a],
+                });
+            }
+
+            return gerechten;
+        }
+    }
+}
