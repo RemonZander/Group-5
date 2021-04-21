5de0eb3c2d33b9d091a423ae071eac617301661a
simon prinsen <simon360@hotmail.nl>
2021-04-21 12:57:14 +0200

    IO.cs communication with database restructure, implemented optimalization with Rémon. changed reservering_beschikbaarheid to not take all days between now and days given, now works with a starting and end date

--- restaurant/restaurant/IO.cs
+++ restaurant/restaurant/IO.cs
@@ -5,7 +5,6 @@ using System.Text;
 using Microsoft.VisualBasic.FileIO;
 using Newtonsoft.Json;
 using System.IO;
-using Microsoft.VisualBasic.FileIO;
 
 namespace restaurant
 {
@@ -19,7 +18,7 @@ namespace restaurant
                 FileSystem.CreateDirectory(@"..\database\");
             }
 
-            
+
             string output = JsonConvert.SerializeObject(database, Formatting.Indented);
             // @ neemt tekst letterlijk, geen \n bijv.
             File.WriteAllText(@"..\database\database.Json", output);
@@ -28,7 +27,7 @@ namespace restaurant
         public Database Getdatabase()
         {
             Database database = new Database();
-            
+
             if (!File.Exists(@"..\database\database.Json")) return database;
             string output = File.ReadAllText(@"..\database\database.Json");
             database = JsonConvert.DeserializeObject<Database>(output);
@@ -50,7 +49,7 @@ namespace restaurant
 
             return database;
         }
-        
+
         //Reset de database
         public void Reset_filesystem()
         {
@@ -67,92 +66,7 @@ namespace restaurant
                 FileSystem.CreateDirectory(@"..\database\");
             }
         }
-
-        //pakt alle beschikbare tijden en tafels tussen nu en aantal ingevoerde dagen
-        public List<Tuple<DateTime, List<Tafels>>> Reservering_beschikbaarheid(int dagen)
-        {
-            database = Getdatabase();
-            //maakt een lijst met tuples die beheert alle beschikbare plekken op int aantal dagen
-            List<Tuple<DateTime, List<Tafels>>> beschikbaar = new List<Tuple<DateTime, List<Tafels>>>();
-
-            //vult de List met alle beschikbare momenten en tafels
-            DateTime possibleTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 10, 0, 0);
-            for (int days = 0; days <= dagen; days++)
-            {
-                //45 kwaterieren van 1000 tot 2100
-                for (int i = 0; i < 45; i++)
-                {
-                    beschikbaar.Add(Tuple.Create(possibleTime, database.tafels));
-                    possibleTime = possibleTime.AddMinutes(15);
-                }
-                //gaat naar de volgende dag met de openingsuren
-                possibleTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1, 10, 0, 0);
-            }
-
-            //verantwoordelijk voor het communiceren met de database
-            foreach (var reservering in database.reserveringen)
-            {
-                //voor de datum tussen nu en de ingevoerde dag
-                if (reservering.datum >= DateTime.Now && reservering.datum <= new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + dagen, 21, 0, 0))
-                {
-                    //temptablelist bevat alle tafels
-                    //List<Tafels> tempTableList = beschikbaar[beschikbaar.IndexOf(Tuple.Create(reservering.datum, database.tafels))].Item2;
-                    List<Tafels> tempTableList = new List<Tafels>();
-                    List<Tafels> removed_tables = new List<Tafels>();
-                    //gaat door alle gereserveerde tafels in die reservering en haalt deze weg
-                    for (int d = 0; d < beschikbaar.Count; d++)
-                    {
-                        if (beschikbaar[d].Item1 == reservering.datum)
-                        {
-                            tempTableList = new List<Tafels>(beschikbaar[d].Item2);
-                            foreach (var tafel in reservering.tafels)
-                            {
-                                tempTableList.Remove(tafel);
-                                removed_tables.Add(tafel);
-                            }
-                            break;
-                        }
-                    }
-
-                    //als er geen tafels meer vrij zijn haalt hij de tafel weg
-                    if (tempTableList.Count == 0)
-                        for (int a = 0; a < beschikbaar.Count; a++)
-                        {
-                            if (beschikbaar[a].Item1 == reservering.datum)
-                            {
-                                for (int b = 0; b <= 8; b++)
-                                {
-                                    beschikbaar.RemoveAt(a + b);
-                                }
-                                break;
-                            }
-                        }
-                    //maakt tuple met tafels die wel beschikbaar zijn
-                    else
-                    {
-                        for (int a = 0; a < beschikbaar.Count; a++)
-                        {
-                            if (beschikbaar[a].Item1 == reservering.datum)
-                            {
-                                beschikbaar[a] = Tuple.Create(reservering.datum, tempTableList);
-                                for (int b = 1; b <= 8; b++)
-                                {
-                                    if ((a + b) >= beschikbaar.Count) break;
-                                    beschikbaar[a + b] = Tuple.Create(reservering.datum.AddMinutes(15 * b), beschikbaar[a + b].Item2.Except(removed_tables).ToList());
-                                    if (beschikbaar[a + b].Item2.Count == 0)
-                                    {
-                                        beschikbaar.RemoveAt(a + b);
-                                    }
-                                }
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-            return beschikbaar;
-        }
-
+        
         //pakt alle beschikbare tijden en tafels voor de ingevoerde dag
         public List<Tuple<DateTime, List<Tafels>>> Reservering_beschikbaarheid(DateTime date)
         {
@@ -162,7 +76,7 @@ namespace restaurant
 
             //vult de List met alle beschikbare momenten en tafels
             DateTime possibleTime = new DateTime(date.Year, date.Month, date.Day, 10, 0, 0);
-            
+
             //45 kwaterieren van 1000 tot 2100
             for (int i = 0; i < 45; i++)
             {
@@ -176,61 +90,99 @@ namespace restaurant
                 //voor de datum tussen nu en de ingevoerde dag
                 if (reservering.datum >= new DateTime(date.Year, date.Month, date.Day, 10, 0, 0) && reservering.datum <= new DateTime(date.Year, date.Month, date.Day, 21, 0, 0))
                 {
-                    //temptablelist bevat alle tafels
-                    //List<Tafels> tempTableList = beschikbaar[beschikbaar.IndexOf(Tuple.Create(reservering.datum, database.tafels))].Item2;
-                    List<Tafels> tempTableList = new List<Tafels>();
-                    List<Tafels> removed_tables = new List<Tafels>();
-                    //gaat door alle gereserveerde tafels in die reservering en haalt deze weg
-                    for (int d = 0; d < beschikbaar.Count; d++)
-                    {
-                        if (beschikbaar[d].Item1 == reservering.datum)
-                        {
-                            tempTableList = new List<Tafels>(beschikbaar[d].Item2);
-                            foreach (var tafel in reservering.tafels)
-                            {
-                                tempTableList.Remove(tafel);
-                                removed_tables.Add(tafel);
-                            }
-                            break;
-                        }
-                    }
+                    beschikbaar = verwijder_Reservering(beschikbaar, reservering);
+                }
+            }
+            return beschikbaar;
+        }
 
-                    //als er geen tafels meer vrij zijn haalt hij de tafel weg
-                    if (tempTableList.Count == 0)
-                        for (int a = 0; a < beschikbaar.Count; a++)
-                        {
-                            if (beschikbaar[a].Item1 == reservering.datum)
-                            {
-                                for (int b = 0; b <= 8; b++)
-                                {
-                                    beschikbaar.RemoveAt(a + b);
-                                }
-                                break;
-                            }
-                        }
-                    //maakt tuple met tafels die wel beschikbaar zijn
-                    else
+        //pakt alle beschikbare tijden en tafels tussen nu en aantal ingevoerde dagen
+        public List<Tuple<DateTime, List<Tafels>>> Reservering_beschikbaarheid(int start_maand, int eind_maand, int start_dag, int eind_dag)
+        {
+            database = Getdatabase();
+            //maakt een lijst met tuples die beheert alle beschikbare plekken op int aantal dagen
+            List<Tuple<DateTime, List<Tafels>>> beschikbaar = Calc_totale_beschikbaarheid(start_maand, eind_maand, start_dag, eind_dag);
+
+
+            //verantwoordelijk voor het communiceren met de database
+            foreach (var reservering in database.reserveringen)
+            {
+                //voor de datum tussen nu en de ingevoerde dag
+                if (reservering.datum >= new DateTime(DateTime.Now.Year, start_maand, start_dag, 10, 0, 0) && reservering.datum <= new DateTime(DateTime.Now.Year, eind_maand, eind_dag, 21, 0, 0))
+                {
+                    beschikbaar = verwijder_Reservering(beschikbaar, reservering);
+                }
+            }
+            return beschikbaar;
+        }
+
+        private List<Tuple<DateTime, List<Tafels>>> verwijder_Reservering(List<Tuple<DateTime, List<Tafels>>> beschikbaar, Reserveringen reservering)
+        {
+            //temptablelist bevat alle tafels
+            //List<Tafels> tempTableList = beschikbaar[beschikbaar.IndexOf(Tuple.Create(reservering.datum, database.tafels))].Item2;
+            List<Tafels> tempTableList = new List<Tafels>();
+            List<Tafels> removed_tables = new List<Tafels>();
+            int location = 0;
+            //gaat door alle gereserveerde tafels in die reservering en haalt deze weg
+            for (int d = 0; d < beschikbaar.Count; d++)
+            {
+                if (beschikbaar[d].Item1 == reservering.datum)
+                {
+                    tempTableList = new List<Tafels>(beschikbaar[d].Item2);
+                    foreach (var tafel in reservering.tafels)
                     {
-                        for (int a = 0; a < beschikbaar.Count; a++)
-                        {
-                            if (beschikbaar[a].Item1 == reservering.datum)
-                            {
-                                beschikbaar[a] = Tuple.Create(reservering.datum, tempTableList);
-                                for (int b = 1; b <= 8; b++)
-                                {
-                                    if ((a + b) >= beschikbaar.Count) break;
-                                    beschikbaar[a + b] = Tuple.Create(reservering.datum.AddMinutes(15 * b), beschikbaar[a + b].Item2.Except(removed_tables).ToList());
-                                    if (beschikbaar[a + b].Item2.Count == 0)
-                                    {
-                                        beschikbaar.RemoveAt(a + b);
-                                    }
-                                }
-                                break;
-                            }
-                        }
+                        tempTableList.Remove(tafel);
+                        removed_tables.Add(tafel);
+                    }
+                    location = d;
+                    break;
+                }
+            }
+
+            //als er geen tafels meer vrij zijn haalt hij de tafel weg
+            if (tempTableList.Count == 0)
+            {
+                beschikbaar.RemoveAt(location);
+            }
+            //maakt tuple met tafels die wel beschikbaar zijn
+            else
+            {
+                beschikbaar[location] = Tuple.Create(reservering.datum, tempTableList);
+                for (int b = 1; b <= 8; b++)
+                {
+                    if ((location + b) >= beschikbaar.Count) break;
+                    beschikbaar[location + b] = Tuple.Create(reservering.datum.AddMinutes(15 * b), beschikbaar[location + b].Item2.Except(removed_tables).ToList());
+                    if (beschikbaar[location + b].Item2.Count == 0)
+                    {
+                        beschikbaar.RemoveAt(location + b);
                     }
                 }
             }
+            return beschikbaar;
+        }
+
+        private List<Tuple<DateTime, List<Tafels>>> Calc_totale_beschikbaarheid(int start_maand, int eind_maand, int start_dag, int eind_dag)
+        {
+            List<Tuple<DateTime, List<Tafels>>> beschikbaar = new List<Tuple<DateTime, List<Tafels>>>();
+
+            //vult de List met alle beschikbare momenten en tafels
+            DateTime possibleTime = new DateTime(DateTime.Now.Year, start_maand, start_dag, 10, 0, 0);
+            for (int maanden = start_maand; maanden <= eind_maand; maanden++)
+            {
+                for (int days = start_dag; days <= eind_dag; days++)
+                {
+                    //gaat naar de volgende dag met de openingsuren
+                    possibleTime = new DateTime(DateTime.Now.Year, maanden, days, 10, 0, 0);
+                    //45 kwaterieren van 1000 tot 2100
+                    for (int i = 0; i < 45; i++)
+                    {
+                        beschikbaar.Add(Tuple.Create(possibleTime, database.tafels));
+                        possibleTime = possibleTime.AddMinutes(15);
+                    }
+                }
+                possibleTime = new DateTime(DateTime.Now.Year, maanden, start_dag, 10, 0, 0);
+            }
+
             return beschikbaar;
         }
     }
