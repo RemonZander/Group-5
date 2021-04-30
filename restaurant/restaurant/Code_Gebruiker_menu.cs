6281bab9b86b3f38be8368811fafe51eab0cfda0
simon prinsen <simon360@hotmail.nl>
2021-04-30 15:04:52 +0200

    finished review and feedback in code_gebruiker_menu

--- restaurant/restaurant/Code_Gebruiker_menu.cs
+++ restaurant/restaurant/Code_Gebruiker_menu.cs
@@ -36,6 +36,7 @@ namespace restaurant
             database = io.Getdatabase();
         }
 
+        #region Reserveringen
         //pakt de reservering van een klant
         public Reserveringen Get_reservation(Klantgegevens klant)
         {
@@ -63,7 +64,7 @@ namespace restaurant
             {
                 database.reserveringen = new List<Reserveringen>();
             }
-                
+
             Reserveringen reservering = new Reserveringen
             {
 
@@ -86,11 +87,12 @@ namespace restaurant
 
         //reset de database
         public void Remove_reservations(Reserveringen reserveringen)
-            {
-                database = io.Getdatabase();
-                database.reserveringen.Remove(reserveringen);
-                io.Savedatabase(database);
-            }
+        {
+            database = io.Getdatabase();
+            database.reserveringen.Remove(reserveringen);
+            io.Savedatabase(database);
+        }
+        #endregion
 
         #region Menukaart
         //pakt menukaart uit de database
@@ -181,7 +183,6 @@ namespace restaurant
         }
         #endregion
 
-
         #region Reviews
         //maakt een review
         public void makeReview(int rating, Klantgegevens klant, string message, Reserveringen reservering, bool anoniem)
@@ -198,6 +199,7 @@ namespace restaurant
                 Klantnummer = klant.klantnummer,
                 message = message,
                 reservering_ID = reservering.ID,
+                anomiem = anoniem,
                 datum = DateTime.Now
             };
             if (database.reviews.Count == 0)
@@ -217,7 +219,7 @@ namespace restaurant
         public List<Review> getReviews()
         {
             io.Getdatabase();
-            List < Review > reviewList = new List<Review>();
+            List<Review> reviewList = new List<Review>();
             if (database.reviews == null)
             {
                 return reviewList;
@@ -243,11 +245,184 @@ namespace restaurant
             }
         }
 
-        //TO DO
-        //public Review overwriteReview(int rating, Klantgegevens klant, string message, Reserveringen reservering, bool anoniem)
-        //{
+        //overwrite een review met 
+        public void overwriteReview(int reviewID, int rating, Klantgegevens klant, string message, bool anoniem)
+        {
+            //pakt de database
+            database = io.Getdatabase();
 
-        //}
+            //voor alle reviews in de database
+            for (int i = 0; i < database.reviews.Count; i++)
+            {
+                //als reviewID in de database gelijk staat aan gegeven reviewID en voor opgegeven klant, moet niet hebben dat mensen andere reviews kunnen aanpassen
+                if (reviewID == database.reviews[i].ID && klant.klantnummer == database.reviews[i].Klantnummer)
+                {
+                    //maakt een nieuwe review op dezelfde locatie als de oude met informatie van de oude en de nieuwe
+                    database.reviews[i] = new Review
+                    {
+                        Rating = rating,
+                        anomiem = anoniem,
+                        datum = database.reviews[i].datum,
+                        ID = database.reviews[i].ID,
+                        Klantnummer = database.reviews[i].Klantnummer,
+                        message = message,
+                        reservering_ID = database.reviews[i].reservering_ID,
+                    };
+                    //stop the count
+                    break;
+                }
+            }
+            //save de database
+            io.Savedatabase(database);
+        }
+
+        //delete een review
+        public void deleteReview(int reviewID, Klantgegevens klant)
+        {
+            //pakt de database
+            database = io.Getdatabase();
+
+            //voor alle reviews in de database
+            for (int i = 0; i < database.reviews.Count; i++)
+            {
+                //als reviewID in de database gelijk staat aan gegeven reviewID en voor opgegeven klant, moet niet hebben dat mensen andere reviews kunnen aanpassen
+                if (reviewID == database.reviews[i].ID && klant.klantnummer == database.reviews[i].Klantnummer)
+                {
+                    //delete de review
+                    database.reviews.RemoveAt(i);
+
+                    //stop the count
+                    break;
+                }
+            }
+            //save de database
+            io.Savedatabase(database);
+        }
+        #endregion
+
+        #region Feedback
+        //maakt feedback aan
+        public void makeFeedback(Werknemer werknemer,Klantgegevens klant, string message, Reserveringen reservering, bool anoniem)
+        {
+            //pakt database
+            database = io.Getdatabase();
+            
+            //als feedback lijst nog niet bestaat maak die aan
+            if (database.feedback == null)
+            {
+                database.feedback = new List<Feedback>();
+            }
+
+            //maakt feedback
+            Feedback feedback = new Feedback
+            {
+                //recipient = 
+                anomiem = anoniem,
+                datum = DateTime.Now,
+                Klantnummer = klant.klantnummer,
+                message = message,
+                reservering_ID = reservering.ID,
+            };
+            
+            //als er geen feedback in de lijst is, zet ID naar 1, anders feebackID is laatste feedbackID+1
+            if (database.feedback.Count == 0)
+            {
+                feedback.ID = 1;
+            }
+            else
+            {
+                feedback.ID = database.feedback[database.feedback.Count - 1].ID + 1;
+            }
+            //slaat de database op
+            io.Savedatabase(database);
+        }
+        
+        //returned 50 items, als er zoveel zijn
+        public List<Feedback> getFeedback()
+        {
+            //pakt de database
+            io.Getdatabase();
+            //maakt een lege feedbacklijst aan
+            List<Feedback> feedbackList = new List<Feedback>();
+            //als er geen feedback is return de lege lijst
+            if (database.feedback == null)
+            {
+                return feedbackList;
+            }
+            else
+            {
+                //sla tot 50 items op en return deze lijst
+                for (int i = 0, j = 0; i < database.feedback.Count && j < 50; i++, j++)
+                {
+                    if (database.feedback[i].anomiem)
+                    {
+                        Feedback temp = database.feedback[i];
+                        temp.Klantnummer = 0;
+                        temp.reservering_ID = 0;
+                        feedbackList.Add(temp);
+                    }
+                    else
+                    {
+                        feedbackList.Add(database.feedback[i]);
+                    }
+                }
+                return feedbackList;
+            }
+        }
+
+        //herschrijft feedback voor gegeven feedbackID
+        public void overwriteFeedback(int feedbackID, Klantgegevens klant, string message, bool anoniem)
+        {
+            //pakt de database
+            database = io.Getdatabase();
+
+            //voor alle feedbacks in de database
+            for (int i = 0; i < database.feedback.Count; i++)
+            {
+                //als feedbackID in de database gelijk staat aan gegeven feedbackID en voor opgegeven klant, moet niet hebben dat mensen andere feedback kunnen aanpassen
+                if (feedbackID == database.feedback[i].ID && klant.klantnummer == database.feedback[i].Klantnummer)
+                {
+                    //maakt een nieuwe feeback op dezelfde locatie als de oude met informatie van de oude en de nieuwe
+                    database.feedback[i] = new Feedback
+                    {
+                        //recipient = 
+                        anomiem = anoniem,
+                        datum = database.feedback[i].datum,
+                        ID = database.feedback[i].ID,
+                        Klantnummer = database.feedback[i].Klantnummer,
+                        message = message,
+                        reservering_ID = database.feedback[i].reservering_ID,
+                    };
+                    //stop the count
+                    break;
+                }
+            }
+            //save de database
+            io.Savedatabase(database);
+        }
+
+        //delete feedback voor gegeven feedbackID
+        public void deleteFeedback(int feedbackID, Klantgegevens klant)
+        {
+            //pakt de database
+            database = io.Getdatabase();
+
+            //voor alle feedback in de database
+            for (int i = 0; i < database.feedback.Count; i++)
+            {
+                //als feedbackID in de database gelijk staat aan gegeven feedbackID en voor opgegeven klant, moet niet hebben dat mensen andere feedback kunnen aanpassen
+                if (feedbackID == database.feedback[i].ID && klant.klantnummer == database.feedback[i].Klantnummer)
+                {
+                    //delete de review
+                    database.feedback.RemoveAt(i);
+
+                    //stop the count
+                    break;
+                }
+            }
+            //save de database
+            io.Savedatabase(database);
+        }
+        #endregion
     }
-    #endregion
 }
