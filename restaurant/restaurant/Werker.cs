09addf4ba5883cbbcff454f46dafcb7d4946751b
Rémon Zander (0948619) <0948619@hr.nl>
2021-06-02 14:13:57 +0200

    added ExpensesScreen and more bugfixes and other stuff

--- restaurant/restaurant/Werker.cs
+++ restaurant/restaurant/Werker.cs
@@ -5,27 +5,15 @@ using System.Text;
 
 namespace restaurant
 {
-    public abstract class Werker : Type_BTW_Prijs
+    public abstract class Werker
     {
-        public double inkomstenbelasting { get; set; }
-
         public double lease_auto { get; set; }
 
-        public double onkostenvergoeding { get; set; }
-
-        public double pensioenpremie { get; set; }
+        public int ID { get; set; }
 
         public double prestatiebeloning { get; set; }
 
         public double salaris { get; set; }
-
-        public double salaris_13e_maand { get; set; }
-
-        public double vakantiegeld { get; set; }
-
-        public double werkkleding { get; set; }
-
-        public double ziektekostenverzekering { get; set; }
 
         public Login_gegevens login_gegevens { get; set; }
     }
